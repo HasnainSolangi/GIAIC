2318dd129e7076f35953024982d526b9162b093f
Hasnain Ahmed <156552529+HasnainSolangi@users.noreply.github.com>
2024-04-09 13:47:39 +0500

    TypeScript/onsite-classes/classes/class002-variables directory

--- TypeScript/onsite-classes/classes/class002-variables/dist/variable.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-let myIntro = "My name is Hasnain Ahmed 25";
-console.log(myIntro);
-console.log(myIntro);
-console.log(myIntro);
--- TypeScript/onsite-classes/classes/class002-variables/src/variable.ts
+++
@@ -1,5 +0,0 @@
-let myIntro = "My name is Hasnain Ahmed 25"
-
-console.log(myIntro); 
-console.log(myIntro); 
-console.log(myIntro); 

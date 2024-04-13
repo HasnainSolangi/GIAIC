9472b82caad79014f5a13f1e667eec6c24e65322
Hasnain Ahmed <156552529+HasnainSolangi@users.noreply.github.com>
2024-04-13 13:55:59 +0500

    TypeScript/onsite-classes/classes/class002-simple-variable-intro directory

--- TypeScript/onsite-classes/classes/class002-simple-variable-intro/dist/intro.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-let myIntro = ("My name is Hasnain Ahmed");
-console.log("myIntro");
-console.log("myIntro");
-console.log("myIntro");

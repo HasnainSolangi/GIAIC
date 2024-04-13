9256ab745df7615e6585c93b2e3d58233e795efe
Hasnain Ahmed <156552529+HasnainSolangi@users.noreply.github.com>
2024-04-13 13:41:27 +0500

    Add files via upload

---
+++ TypeScript/onsite-classes/classes/class002-simple-variable-intro/dist/intro.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let myIntro = ("My name is Hasnain Ahmed");
+console.log("myIntro");
+console.log("myIntro");
+console.log("myIntro");

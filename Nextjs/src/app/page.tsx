54874717cbfce9ade3ebe2fd4ce7dbab34ee24aa
Hasnain Ahmed <156552529+HasnainSolangi@users.noreply.github.com>
2024-10-18 03:11:45 +0500

    Upload class 01 files

---
+++ Nextjs/src/app/page.tsx
@@ -0,0 +1,10 @@
+function Homepage()
+{
+  return (
+    <>
+      <h1>Assalamu Alaikum,</h1>
+      <p>Hello, World! I'm Hasnain Ahmed, and I'm diving into Next.js!</p>
+    </>
+  )
+}
+export default Homepage
